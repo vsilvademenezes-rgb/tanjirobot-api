d797c00641515556d1c1bfea32b5f48eacdbf311
vsilvademenezes-rgb <vsilvademenezes@gmail.com>
2026-05-10 04:34:40 -0300

    Update index.js

--- ship/index.js
+++ ship/index.js
@@ -56,9 +56,10 @@ app.get("/ship", async (req, res) => {
             return await loadImage(b);
         }
 
-        const [imgA1, imgA2] = await Promise.all([
+        const [imgA1, imgA2, imgNezuko] = await Promise.all([
             getImg(a1).catch(() => getImg("https://cdn.discordapp.com/embed/avatars/0.png")),
-            getImg(a2).catch(() => getImg("https://cdn.discordapp.com/embed/avatars/1.png"))
+            getImg(a2).catch(() => getImg("https://cdn.discordapp.com/embed/avatars/1.png")),
+            getImg("https://i.postimg.cc/02Pz1nZx/1778398183228.png")
         ]);
 
         // Fundo gradiente
@@ -105,7 +106,10 @@ app.get("/ship", async (req, res) => {
         ctx.arc(510, 100, 70, 0, Math.PI * 2);
         ctx.stroke();
 
-        // Coração maior no meio
+        // Nezuko atrás do coração
+        ctx.drawImage(imgNezuko, 230, -10, 150, 210);
+
+        // Coração na frente da Nezuko
         ctx.save();
         ctx.translate(305, 72);
         ctx.strokeStyle = "white";
@@ -119,7 +123,7 @@ app.get("/ship", async (req, res) => {
         ctx.stroke();
         ctx.restore();
 
-        // Porcentagem centralizada dentro do coração
+        // Porcentagem dentro do coração
         ctx.fillStyle = "white";
         ctx.font = `bold 24px ${fonte}`;
         ctx.textAlign = "center";
